9aa3f0fc4ac2f1ae4f88309086cd458f62780e3f
Megan Stiles <megan.e.stiles@gmail.com>
2019-07-03 15:47:46 -0400

    added total payment amounts

--- src/paymentBreakdown.py
+++ src/paymentBreakdown.py
@@ -32,3 +32,22 @@ def getInterestPayment (payment_amount, principle_balance, interest_rate):
     interest_payment = principle_balance * (interest_rate/12)
 
     return interest_payment
+
+def getTotalPrincipleInterest(payment_amount, principle_balance, interest_rate, num_payments):
+
+    total_principle = 0
+    total_interest = 0
+
+    for i in range (0, num_payments):
+
+        principle = get_payment_principle(payment_amount, principle_balance, interest_rate)
+
+        interest = get_interest_payment(payment_amount, principle_balance, interest_rate)
+
+        total_principle = total_principle + principle
+
+        total_interest = total_interest + interest
+
+        principle_balance = principle_balance - principle
+
+    return total_principle, total_interest
